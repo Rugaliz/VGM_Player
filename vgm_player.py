6d7e3b782b7afc979c8528d7005fef3e929dccf7
Rugaliz <37263443+Rugaliz@users.noreply.github.com>
2022-05-09 22:20:30 +0100

    now works without argument

--- vgm_player.py
+++ vgm_player.py
@@ -2,36 +2,24 @@ import os
 import sys
 from random import randrange
 
-walk_dir = sys.argv[1]
+try:
+    work_dir = sys.argv[1]
+except:
+    work_dir = "."
 
-# print('walk_dir = ' + walk_dir)
-
-# If your current working directory may change during script execution, it's recommended to
-# immediately convert program arguments to an absolute path. Then the variable root below will
-# be an absolute path as well. Example:
-# walk_dir = os.path.abspath(walk_dir)
-print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
+print('work_dir (absolute) = ' + os.path.abspath(work_dir))
 vgm_list = []
-for root, subdirs, files in os.walk(walk_dir):
-    # print('--\nroot = ' + root)
+
+for root, subdirs, files in os.walk(work_dir):
     list_file_path = os.path.join(root, 'my-directory-list.txt')
     # print('list_file_path = ' + list_file_path)
 
-    with open(list_file_path, 'wb') as list_file:
-        # for subdir in subdirs:
-            # print('\t- subdirectory ' + subdir)
+    for filename in files:
+        file_path = os.path.join(root, filename)
+        if ".vgz" in file_path or ".vgm" in file_path:
+            # print(file_path)
+            vgm_list.append(file_path)
 
-        for filename in files:
-            file_path = os.path.join(root, filename)
-            if ".vgz" in file_path or ".vgm" in file_path:
-                # print('\t- file %s (full path: %s)' % (filename, file_path))
-                print(file_path)
-                vgm_list.append(file_path)
-            # with open(file_path, 'rb') as f:
-            #     f_content = f.read()
-            #     list_file.write(('The file %s contains:\n' % filename).encode('utf-8'))
-            #     list_file.write(f_content)
-            #     list_file.write(b'\n')
 nsong = len(vgm_list)
 
 if nsong == 0:
@@ -39,6 +27,8 @@ if nsong == 0:
 
 while nsong > 0:
     song_index = randrange(nsong)
-    os.system(f"env SDL_VIDEODRIVER=dummy mame vgmplay -quik '{vgm_list[song_index]}'  -video none -seconds_to_run 180")
+    print(f"Playing {vgm_list[song_index]}")
+    os.system(f"env SDL_VIDEODRIVER=dummy mame vgmplay -quik \"{vgm_list[song_index]}\"  -video none -seconds_to_run 180")
     vgm_list.remove(vgm_list[song_index])
     nsong = len(vgm_list)
+    print(f"{nsong} songs remaining")
